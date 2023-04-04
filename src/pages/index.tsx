69376ad90676c20eb454ef6a2f2599c5ada416d3
Alikulov <ramoalikulov77@gmail.com>
2023-04-04 17:36:07 +0600

    build project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import Home from '@/components/screens/home/Home'
 import { IMentorData } from '@/types/mentor.interface'
 import { GetStaticProps, NextPage } from 'next'
-import mentorService from '../services/mentor.services'
+import MentorService from '../services/mentor.services'
 
 const HomePage: NextPage<IMentorData> = ({ mentors }) => {
 	return <Home mentors={mentors} />
 }
 
 export const getServerSideProps: GetStaticProps<IMentorData> = async () => {
-	const mentors = await mentorService.getAll()
+	const mentors = await MentorService.getAll()
 	return {
 		props: { mentors },
 	}
